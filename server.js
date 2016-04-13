1ac0df2f0abad25e46f45dfbca6d6c6005866ad2
Vijay Chhuttani <chhuttani.vijay@gmail.com>
2016-04-14 04:35:03 +0530

    Initial commit of Application: created API to publish random quotes based on keywords provided as input

---
+++ server.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+//ROUTES
+
+app.get('/quotes/:keyword', function(req, res) {
+	if(typeof req.params.keyword !== 'undefined' && null !== req.params.keyword){
+		getRandomQuote(req.params.keyword, function(data){
+			if(data !== '404') {
+				res.status(200).send(data);
+			} else {
+				res.send("Oops!! I do not know any quote having keyword(s) '" 
+					+ req.params.keyword 
+					+ "', Let's try something else!");
+			}
+			
+		});
+	} else {
+		res.send("Invalid Keyword");
+	}
+});
+
+//SETTING STATIC SITE AND STARTING SERVER
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function() {
+	console.log('Server started at port ' + PORT);
+});
+
+//UTILITY FUNCTIONS
+
+function getRandomQuote(keyword, callback) {
+	var pageNum = Math.floor(Math.random() * (10)) + 1;
+	var url = 'http://www.brainyquote.com/search_results.html?q=' + keyword + '&pg=' + pageNum;
+	var quotes = [];
+	request(url, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    var $ = cheerio.load(response.body);
+		$('.bqQuoteLink').each(function(i, elem) {
+			quotes[i] = $(this).find('a').text();
+		});
+		if(quotes.length === 0) {
+			callback("404");
+		}
+		var quoteNum = Math.floor(Math.random() * (quotes.length));
+		console.log("URL: " + url + " ---> QuoteNumber: " + quoteNum);
+		console.log("Quote: " + quotes[quoteNum]);
+		callback(quotes[quoteNum]);
+	  } else {
+	  	callback("Error 500 : Some error occured, please try later.");
+	  }
+	});
+}
+
+
