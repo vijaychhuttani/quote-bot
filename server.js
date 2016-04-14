9b3fbab1d6e512d5649cb29c4d0b4ed60a9a6217
Vijay Chhuttani <chhuttani.vijay@gmail.com>
2016-04-15 01:49:41 +0530

    removed logger from req

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/webhook/', function (req, res) {
 app.post('/webhook/', function (req, res) {
 	console.log(req);
 	if(typeof req.body !== 'undefined' && req.body !== null){
-		console.log("VIJAY TEST - " + req.body.entry);
+		//console.log("VIJAY TEST - " + req.body.entry);
 	  messaging_events = req.body.entry[0].messaging;
 	  for (i = 0; i < messaging_events.length; i++) {
 	    event = req.body.entry[0].messaging[i];
