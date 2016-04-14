c1b485ef7b3d86660c7c6ff3abb34b6d306a10b4
Vijay Chhuttani <chhuttani.vijay@gmail.com>
2016-04-14 11:51:00 +0530

    Fixed null pointer in webhook in case of empty request body

--- server.js
+++ server.js
@@ -14,17 +14,21 @@ app.get('/webhook/', function (req, res) {
 });
 
 app.post('/webhook/', function (req, res) {
-  console.log("VIJAY TEST - " + req.body.entry);
-  messaging_events = req.body.entry[0].messaging;
-  for (i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i];
-    sender = event.sender.id;
-    if (event.message && event.message.text) {
-      text = event.message.text;
-      console.log(sender + ': ' + text);
-    }
-  }
-  res.sendStatus(200);
+	if(typeof req.body !== 'undefined' && req.body !== null){
+		console.log("VIJAY TEST - " + req.body.entry);
+	  messaging_events = req.body.entry[0].messaging;
+	  for (i = 0; i < messaging_events.length; i++) {
+	    event = req.body.entry[0].messaging[i];
+	    sender = event.sender.id;
+	    if (event.message && event.message.text) {
+	      text = event.message.text;
+	      console.log(sender + ': ' + text);
+	    }
+	  }
+	} else {
+		console.log("Empty request in POST:/webhook");
+	}
+	res.sendStatus(200);
 });
 
 app.get('/init', function(req, res){
